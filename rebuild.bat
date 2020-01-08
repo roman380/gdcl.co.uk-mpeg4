b26143d688ac4aeada08195f3072bf8545169b09
Roman Ryltsov <roman@alax.info>
2020-01-08 15:13:50 +0200

    Updated binaries and installer; added build batch file

---
+++ rebuild.bat
@@ -0,0 +1,7 @@
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+@set T=Rebuild
+for %%P in (Win32 x64) do (
+	for %%C in (Debug Release) do (
+		msbuild mp4.sln /t:%T% /p:Platform="%%~P";Configuration="%%~C"
+	)
+)
