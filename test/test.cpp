1c5014f28d39524ca6fc2133f2d090616075a4e8
Roman Ryltsov <roman@alax.info>
2022-09-26 19:38:22 +0200

    Added wil; added framework unit test project

---
+++ test/test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Test
+{
+	TEST_CLASS(Mp4File)
+	{
+	public:
+		
+		TEST_METHOD(Temporary)
+		{
+		}
+	};
+}
