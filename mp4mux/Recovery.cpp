a188f12cdf1036a679bd91e9e905ef5b6218a14d
Roman Ryltsov <roman@alax.info>
2022-12-09 20:23:59 +0100

    Added stub for MuxFilterRecovery COM interface

---
+++ mp4mux/Recovery.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+
+#include <unknwn.h>
+#include <winrt\base.h>
+#include <winrt\Windows.Foundation.h>
+
+#pragma comment(lib, "windowsapp.lib")
+#pragma comment(lib, "runtimeobject.lib")
+
+#include <wil\resource.h>
+#include <wil\com.h>
+#include <wil\winrt.h>
+
+#include "Recovery.h"
---
+++ mp4mux/Recovery.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "mp4mux_h.h"
+
+class __declspec(uuid("{73D9D53D-30A3-451E-976A-2B4186FE27EC}")) MuxFilterRecovery : 
+    public winrt::implements<MuxFilterRecovery, IMuxFilterRecovery>
+{
+public:
+	// IMuxFilterRecovery
+	IFACEMETHOD(Initialize)(IMuxFilterRecoverySite* Site, BSTR Path) override
+	{
+		//TRACE(L"this 0x%p\n", this);
+		try
+		{
+			THROW_HR_IF_NULL(E_INVALIDARG, Path);
+			[[maybe_unused]] auto&& DataLock = m_DataMutex.lock_exclusive();
+			m_Site = Site;
+			m_Path = Path;
+		}
+		CATCH_RETURN();
+		return S_OK;
+	}
+	IFACEMETHOD(Needed)(BOOL* Needed) override
+	{
+		//TRACE(L"this 0x%p\n", this);
+		try
+		{
+			THROW_HR_IF_NULL(E_POINTER, Needed);
+		}
+		CATCH_RETURN();
+		return S_OK;
+	}
+	IFACEMETHOD(Active)(BOOL* Active) override
+	{
+		//TRACE(L"this 0x%p\n", this);
+		try
+		{
+			THROW_HR_IF_NULL(E_POINTER, Active);
+		}
+		CATCH_RETURN();
+		return S_OK;
+	}
+	IFACEMETHOD(Start)() override
+	{
+		//TRACE(L"this 0x%p\n", this);
+		try
+		{
+		}
+		CATCH_RETURN();
+		return S_OK;
+	}
+	IFACEMETHOD(Stop)() override
+	{
+		//TRACE(L"this 0x%p\n", this);
+		try
+		{
+		}
+		CATCH_RETURN();
+		return S_OK;
+	}
+
+private:
+	mutable wil::srwlock m_DataMutex;
+	wil::com_ptr<IMuxFilterRecoverySite> m_Site;
+	std::wstring m_Path;
+};
